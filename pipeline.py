14dc71a1a325d42b61684e27d87221dc68e84b0c
Victoria Murphy <murphyv@uw.edu>
2024-05-23 11:59:39 -0700

    added code structure comments

--- pipeline.py
+++ pipeline.py
@@ -1,2 +1,26 @@
 import numpy as np
 import pandas as pd
+
+## 1) Join housing data, GDP, mortage rates, create boolean column for election year
+
+
+
+
+## 2) Cleaning data: check null, negative values
+
+
+
+
+## 3) LASSO regression model
+
+
+
+
+## 4) Analysis, data viz, r-squared for other states (running LASSO regression for each state, comparing how well model works)
+
+
+
+
+## 5) Creating presentation, 
+
+
